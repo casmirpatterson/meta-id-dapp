a2384412b29033c73b2fdea10d546da5a4b7f539
lukehedger <luke.hedger@gmail.com>
2017-11-28 15:28:05 +0000

    Update isDevelopment util to use constant NODE_ENV

--- app/core/util/index.js
+++ app/core/util/index.js
@@ -1,6 +1,8 @@
+import { NODE_ENV } from 'core/constants'
+
 /**
  * Check for development mode
  *
  * @type {Boolean}
  */
-export const isDevelopment = process.env.NODE_ENV === 'development'
+export const isDevelopment = NODE_ENV === 'development'
