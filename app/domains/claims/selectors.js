5487f881d4154449dfe7ee0e8b0320267e539959
lukehedger <luke.hedger@gmail.com>
2017-11-30 16:49:15 +0000

    Add identity/claims selectors

--- app/domains/claims/selectors.js
+++ app/domains/claims/selectors.js
@@ -1,13 +1,41 @@
+import { createSelector } from 'reselect'
+
 import { name } from './constants'
+import { seletors as Identity } from 'domains/identity'
 
 /**
  * Select the entire domain from the store by `name`
- * 
+ *
  * @param  {Object} state Redux store
  * @return {Object}       Domain state
  */
 const getAll = state => state.get(name)
 
-export default {
+/**
+ * Get all claims where META-ID is the `subject`
+ *
+ * @return {Array}
+ */
+const getClaimsAboutIdentity = createSelector(
+  [getAll, Identity.getIdentityById],
+  (claims, identity) => {
+    return identity.claimsReceived.map(claim => claims[claim])
+  }
+)
 
+/**
+ * Get all claims where META-ID is the `issuer`
+ *
+ * @type {Array}
+ */
+const getClaimsByIdentity = createSelector(
+  [getAll, Identity.getIdentityById],
+  (claims, identity) => {
+    return identity.claimsIssued.map(claim => claims[claim])
+  }
+)
+
+export default {
+  claimsAboutIdentity: getClaimsAboutIdentity,
+  claimsByIdentity: getClaimsByIdentity,
 }
