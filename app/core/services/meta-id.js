a0fd60c2d7fcb22f98aa3f5181e99561f334325d
lukehedger <luke.hedger@gmail.com>
2017-11-30 16:02:03 +0000

    Add identity GraphQL query to meta-id service - fixes #7

--- app/core/services/meta-id.js
+++ app/core/services/meta-id.js
@@ -7,4 +7,26 @@ import MetaNetwork from 'core/services/meta-network'
  * @param  {Object} variables GraphQL query variables
  * @return {Object}           Response data
  */
-const metaNetworkRequest = (query, variables) => MetaNetwork.request(query, variables)
+const metaNetworkRequest = (query, variables) =>
+  MetaNetwork.request(query, variables)
+
+/**
+ * Read a META Identity from the META Identity Index
+ *
+ * @param  {Object} variables       [description]
+ * @param  {String} variables.owner [description]
+ * @return {Object}                 Response data
+ */
+export const readIdentity = variables =>
+  metaNetworkRequest(
+    `
+  query readIdentity($owner: String!) {
+    identity(owner: $owner) {
+      id
+      owner
+      signature
+    }
+  }
+`,
+    variables
+  )
