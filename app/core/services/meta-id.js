aaa08ac8b935806d8e7f93d64c923d3685a5b029
lukehedger <luke.hedger@gmail.com>
2017-11-30 15:55:30 +0000

    Add createIdentity GraphQL mutation to meta-id service - fixes #3

--- app/core/services/meta-id.js
+++ app/core/services/meta-id.js
@@ -7,4 +7,31 @@ import MetaNetwork from 'core/services/meta-network'
  * @param  {Object} variables GraphQL query variables
  * @return {Object}           Response data
  */
-const metaNetworkRequest = (query, variables) => MetaNetwork.request(query, variables)
+const metaNetworkRequest = (query, variables) =>
+  MetaNetwork.request(query, variables)
+
+/**
+ * Add a new META Identity to the META Identity Index
+ *
+ * @param  {Object} variables           [description]
+ * @param  {String} variables.owner     [description]
+ * @param  {String} variables.signature [description]
+ * @param  {String} variables.username  [description]
+ * @return {Object}                     Response data
+ */
+export const createIdentity = variables =>
+  metaNetworkRequest(
+    `
+  mutation CreateIdentity(
+    $username: String,
+    $owner: String,
+    $signature: String
+  ) {
+    createIdentity(username: $username, owner: $owner, signature: $signature) {
+      id
+      owner
+    }
+  }
+`,
+    variables
+  )
