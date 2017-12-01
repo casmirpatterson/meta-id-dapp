58b19168660d22cc6a5fd998adfd3cacafa4bb40
lukehedger <luke.hedger@gmail.com>
2017-12-01 13:19:19 +0000

    Add session domain to store

--- app/core/reducers/index.js
+++ app/core/reducers/index.js
@@ -2,8 +2,10 @@ import { foundReducer } from 'found'
 import { combineReducers } from 'redux-immutablejs'
 
 import * as Identity from 'domains/identity'
+import * as Session from 'domains/session'
 
 export default combineReducers({
   found: foundReducer,
   [Identity.name]: Identity.reducer,
+  [Session.name]: Session.reducer,
 })
