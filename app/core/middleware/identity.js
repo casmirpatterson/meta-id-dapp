2d4faffbccf1bb525c22beaa735b96feead9b59a
lukehedger <luke.hedger@gmail.com>
2017-12-01 13:38:55 +0000

    Add identity middleware: - Read claims on read identity success

---
+++ app/core/middleware/identity.js
@@ -0,0 +1,19 @@
+import { hasAsyncActionSucceeded, isDomainAction } from 'core/util'
+import { actions } from 'domains/claims'
+import { actionTypes as identity, name } from 'domains/identity'
+
+const IdentityMiddleware = ({ dispatch }) => next => action => {
+  // action not in namespace? abort!
+  if (!isDomainAction(name, action.type)) return next(action)
+
+  if (
+    identity.READ_IDENTITY === action.type &&
+    hasAsyncActionSucceeded(action)
+  ) {
+    dispatch(actions.readClaimsBySubject(action.payload.identity[0].owner))
+  }
+
+  return next(action)
+}
+
+export default IdentityMiddleware
