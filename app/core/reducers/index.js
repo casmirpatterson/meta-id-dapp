68c7339c10d420aff9d4b69d895d3068d5821ba3
lukehedger <luke.hedger@gmail.com>
2017-12-01 13:26:17 +0000

    Add claims domain to store

--- app/core/reducers/index.js
+++ app/core/reducers/index.js
@@ -1,11 +1,13 @@
 import { foundReducer } from 'found'
 import { combineReducers } from 'redux-immutablejs'
 
+import * as Claims from 'domains/claims'
 import * as Identity from 'domains/identity'
 import * as Session from 'domains/session'
 
 export default combineReducers({
   found: foundReducer,
+  [Claims.name]: Claims.reducer,
   [Identity.name]: Identity.reducer,
   [Session.name]: Session.reducer,
 })
