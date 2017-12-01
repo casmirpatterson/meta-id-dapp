06f575df2e34d6fc7b1a09d7f94dfd5606d052a5
lukehedger <luke.hedger@gmail.com>
2017-12-01 12:03:04 +0000

    Add skeleton Home page

--- app/pages/Home/Home.js
+++ app/pages/Home/Home.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
 
+import { Link } from 'core/components'
+import { routes } from 'core/routes'
+import * as Components from './components'
+
 class Home extends Component {
   render() {
     return (
       <div>
         <h1>META-ID ÐApp</h1>
+
+        <Components.Search />
+
+        <Link to={routes.register.path}>
+          <button>Register</button>
+        </Link>
+
+        <Link to={routes.login.path}>
+          <button>Login</button>
+        </Link>
       </div>
     )
   }
