d7cbd85f9f841a1efe99e24c1206857462cd3b81
lukehedger <luke.hedger@gmail.com>
2017-12-01 13:00:09 +0000

    Add readIdentity action to Search page

--- app/pages/Search/Search.js
+++ app/pages/Search/Search.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { createStructuredSelector } from 'reselect'
+
+import {
+  actions as IdentityActions,
+  selectors as IdentitySelectors,
+} from 'domains/identity'
 
 class Search extends Component {
+  componentDidMount() {
+    const { actions, routeParams } = this.props
+
+    return actions.readIdentity(routeParams.id)
+  }
+
   render() {
+    const { identity } = this.props
+
     return (
       <div>
         <h2>Search</h2>
+        {identity && <p>Ethereum Address: {identity.owner}</p>}
       </div>
     )
   }
 }
 
-export default Search
+export default connect(
+  createStructuredSelector({
+    identity: IdentitySelectors.identityById,
+  }),
+  dispatch => ({
+    actions: bindActionCreators({ ...IdentityActions }, dispatch),
+  })
+)(Search)
