901bd22e43228a794c7bc50ce9733a474a1f4379
lukehedger <luke.hedger@gmail.com>
2017-12-01 13:21:41 +0000

    Add claim button to Search page if isSessionAccount

--- app/pages/Search/Search.js
+++ app/pages/Search/Search.js
@@ -3,10 +3,13 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { createStructuredSelector } from 'reselect'
 
+import { Link } from 'core/components'
+import { routes } from 'core/routes'
 import {
   actions as IdentityActions,
   selectors as IdentitySelectors,
 } from 'domains/identity'
+import { selectors as SessionSelectors } from 'domains/session'
 
 class Search extends Component {
   componentDidMount() {
@@ -16,13 +19,18 @@ class Search extends Component {
   }
 
   render() {
-    const { identity, routeParams } = this.props
+    const { identity, isSessionAccount, routeParams } = this.props
 
     return (
       <div>
         <h2>Search</h2>
         <p>{routeParams.id}</p>
         {identity && <p>Ethereum Address: {identity.owner}</p>}
+        {isSessionAccount && (
+          <Link to={`${routes.claim.path}/${routeParams.id}`}>
+            <button>New Claim</button>
+          </Link>
+        )}
       </div>
     )
   }
@@ -31,6 +39,7 @@ class Search extends Component {
 export default connect(
   createStructuredSelector({
     identity: IdentitySelectors.identityById,
+    isSessionAccount: SessionSelectors.isSessionAccount,
   }),
   dispatch => ({
     actions: bindActionCreators({ ...IdentityActions }, dispatch),
