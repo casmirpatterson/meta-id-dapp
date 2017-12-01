a91338342952b7333347864ac25924e0c1fba1be
lukehedger <luke.hedger@gmail.com>
2017-12-01 13:52:25 +0000

    Update claims domain selectors to fix getClaimsBySubject

--- app/domains/claims/selectors.js
+++ app/domains/claims/selectors.js
@@ -1,4 +1,7 @@
+import { createSelector } from 'reselect'
+
 import { name } from './constants'
+import { selectors as Identity } from 'domains/identity'
 
 /**
  * Select the entire domain from the store by `name`
@@ -11,15 +14,16 @@ const getAll = state => state.get(name)
 /**
  * Get all claims where META-ID is the `subject`
  *
- * @todo - need to test the efficiency of this selector
- * @see - https://github.com/reactjs/reselect/blob/master/README.md#accessing-react-props-in-selectors
- *
  * @param  {Object} state Redux store
  * @param  {Object} props React component props
  * @return {Array}        Claims about META-ID
  */
-const getClaimsBySubject = (state, { match: { params } }) =>
-  state.get(name).map(claim => claim.subject === params.id)
+const getClaimsBySubject = createSelector(
+  [getAll, Identity.identityById],
+  (claims, identity) => {
+    return claims.filter(claim => claim.subject === identity.owner).toArray()
+  }
+)
 
 export default {
   claimsBySubject: getClaimsBySubject,
