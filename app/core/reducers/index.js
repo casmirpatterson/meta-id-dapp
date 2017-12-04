f1f7c5f4dbf44b2981178db386f5ee1772f3264b
lukehedger <luke.hedger@gmail.com>
2017-12-04 18:49:26 +0000

    Add ui domain to store

--- app/core/reducers/index.js
+++ app/core/reducers/index.js
@@ -4,10 +4,12 @@ import { combineReducers } from 'redux-immutablejs'
 import * as Claims from 'domains/claims'
 import * as Identity from 'domains/identity'
 import * as Session from 'domains/session'
+import * as UI from 'domains/ui'
 
 export default combineReducers({
   found: foundReducer,
   [Claims.name]: Claims.reducer,
   [Identity.name]: Identity.reducer,
   [Session.name]: Session.reducer,
+  [UI.name]: UI.reducer,
 })
