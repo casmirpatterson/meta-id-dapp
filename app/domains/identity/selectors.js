072eacfa27e18819c83d747776a92f566011fc70
lukehedger <luke.hedger@gmail.com>
2017-12-04 16:11:07 +0000

    Update identity selector to accept owner or username

--- app/domains/identity/selectors.js
+++ app/domains/identity/selectors.js
@@ -1,3 +1,5 @@
+import { isValidAddress } from 'ethereumjs-util'
+
 import { MetaId } from 'core/services'
 import { name } from './constants'
 
@@ -10,7 +12,7 @@ import { name } from './constants'
 const getAll = state => state.get(name)
 
 /**
- * Select a META Identity by `id`
+ * Select a META Identity by `id` or `owner` address
  *
  * @todo - need to test the efficiency of this selector
  * @see - https://github.com/reactjs/reselect/blob/master/README.md#accessing-react-props-in-selectors
@@ -20,7 +22,18 @@ const getAll = state => state.get(name)
  * @return {Object}       META Identity
  */
 const getIdentityById = (state, { match: { params } }) => {
-  return state.getIn([name, MetaId.getMetaIdFromUsername(params.id)])
+  let identity
+
+  // check `id` route parameter for `owner` address or `username` string
+  if (isValidAddress(params.id)) {
+    // select identity by `owner` address
+    identity = state.get(name).find(identity => identity.owner === params.id)
+  } else {
+    // select identity by `id` (`username` hash)
+    identity = state.getIn([name, MetaId.getMetaIdFromUsername(params.id)])
+  }
+
+  return identity
 }
 
 export default {
