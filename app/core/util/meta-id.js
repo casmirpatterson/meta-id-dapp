038c5d38f29f5c91cd5f5edb28957d629ff8cfeb
lukehedger <luke.hedger@gmail.com>
2017-12-04 17:54:01 +0000

    Add createMetaClaimObject meta-id util

--- app/core/util/meta-id.js
+++ app/core/util/meta-id.js
@@ -9,6 +9,22 @@ import { bufferToHex, sha3 } from 'ethereumjs-util'
 export const getMetaIdFromUsername = username => bufferToHex(sha3(username))
 
 /**
+ * Utility function to create a valid META Claim object to verify a new claim
+ *
+ * @param  {Object} account      Ethereum account object
+ * @param  {Object} claimMessage Raw claim message
+ * @return {Object}              META Claim object
+ */
+export const createMetaClaimObject = (account, claimMessage) => {
+  return {
+    address: account.address,
+    claimHash: sha3(claimMessage),
+    claimMessage: claimMessage,
+    signature: account.sign(claimMessage),
+  }
+}
+
+/**
  * Utility function to create a valid META-ID object to register a new identity
  *
  * @param  {Object} account  Ethereum account object
