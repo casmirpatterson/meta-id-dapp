1afe685f87ccff4f75266069cc5a24cb818a1f2b
lukehedger <luke.hedger@gmail.com>
2017-12-06 16:17:31 +0000

    Add Spotify API constants

---
+++ app/core/constants/spotify.js
@@ -0,0 +1,4 @@
+export const CLIENT_ID = 'ea4616a7bf3e4e249891d9adf0012e15'
+export const REDIRECT_URI = 'http://localhost:3000/claim/spotify'
+export const SCOPE = 'user-read-private'
+export const SHOW_DIALOG = true
