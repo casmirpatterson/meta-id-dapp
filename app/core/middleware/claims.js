6abf0217812c23f9d683a1c381aabaa42411a4ed
lukehedger <luke.hedger@gmail.com>
2017-12-07 11:10:01 +0000

    Add redirect on claim creation error

--- app/core/middleware/claims.js
+++ app/core/middleware/claims.js
@@ -1,12 +1,20 @@
 import { Actions as farce } from 'farce'
 
 import { routes } from 'core/routes'
-import { hasAsyncActionSucceeded, isDomainAction } from 'core/util'
+import {
+  hasAsyncActionFailed,
+  hasAsyncActionSucceeded,
+  isDomainAction,
+} from 'core/util'
 import { actionTypes as claims } from 'domains/claims'
 
 const ClaimsMiddleware = ({ dispatch }) => next => action => {
   // action not in namespace? abort!
   if (!isDomainAction(name, action.type)) return next(action)
+
+  if (claims.CREATE_CLAIM === action.type && hasAsyncActionFailed(action)) {
+    dispatch(farce.push(`${routes.claim.path}`))
+  }
 
   if (claims.CREATE_CLAIM === action.type && hasAsyncActionSucceeded(action)) {
     dispatch(
