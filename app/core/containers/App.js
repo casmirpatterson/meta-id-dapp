2229e26667714ff85c765d1685d51a7b8bd2ae6b
lukehedger <luke.hedger@gmail.com>
2017-12-07 17:52:33 +0000

    Add router context to App container

--- app/core/containers/App.js
+++ app/core/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { createStructuredSelector } from 'reselect'
@@ -14,6 +15,12 @@ import {
 } from 'domains/session'
 
 class App extends Component {
+  getChildContext() {
+    return {
+      router: this.props.router,
+    }
+  }
+
   render() {
     const { actions, children, sessionIdentity } = this.props
 
@@ -69,6 +76,10 @@ class App extends Component {
       </ThemeProvider>
     )
   }
+}
+
+App.childContextTypes = {
+  router: PropTypes.object,
 }
 
 export default connect(
