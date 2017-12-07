7bffeda4a43b708bf4ed2227c7f5aa609670bb4f
lukehedger <luke.hedger@gmail.com>
2017-12-07 15:35:10 +0000

    Update logo component

--- app/core/components/Logo/Logo.js
+++ app/core/components/Logo/Logo.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import { Image } from 'jaak-primitives'
 
-const Logo = ({ size = ['114px', 'auto'] }) => (
-  <Image backgroundSize="contain" size={size} src="/img/meta-id.png" />
+const Logo = ({ maxWidth = 'auto', size = ['114px', 'auto'] }) => (
+  <Image
+    backgroundSize="contain"
+    margin={[0, 'auto']}
+    size={size}
+    style={{ maxWidth }}
+    src="/img/meta-id.png"
+  />
 )
 
 export default Logo
