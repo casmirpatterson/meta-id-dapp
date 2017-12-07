39b3b8f8f9d98f120086bea12f86a2d06c4edd12
lukehedger <luke.hedger@gmail.com>
2017-12-07 15:24:37 +0000

    Add basic style modules

---
+++ app/core/style/theme.js
@@ -0,0 +1,16 @@
+import { theme } from 'jaak-primitives'
+
+/**
+ * Custom theme
+ * Can be used with styled-components `<ThemeProvider>` 💅
+ *
+ * @see https://www.styled-components.com/docs/advanced#theming
+ *
+ * @type {Object}
+ */
+const customTheme = {
+  ...theme,
+  primary: '#251E2F',
+}
+
+export default customTheme
--- app/index.js
+++ app/index.js
@@ -3,6 +3,8 @@ import { render } from 'react-dom'
 
 import Root from 'core/containers/Root'
 
+import 'core/style/global'
+
 const rootEl = document.getElementById('Root')
 
 render(<Root />, rootEl)
