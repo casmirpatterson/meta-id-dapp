a3c955d1e3a1840772d6efde83d6a50d99c10edf
lukehedger <luke.hedger@gmail.com>
2017-12-08 15:39:53 +0000

    Update Search page styles

--- app/pages/Search/Search.js
+++ app/pages/Search/Search.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import { isValidAddress } from 'ethereumjs-util'
+import { Box } from 'jaak-primitives'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { createStructuredSelector } from 'reselect'
 
 import { Link } from 'core/components'
+import { Button, Card, Image, Text, View } from 'core/primitives'
 import { routes } from 'core/routes'
+import { metaId } from 'core/util'
 import { selectors as ClaimsSelectors } from 'domains/claims'
 import {
   actions as IdentityActions,
@@ -34,29 +37,70 @@ class Search extends Component {
     const { claims, identity, isSessionAccount, routeParams } = this.props
 
     return (
-      <div>
-        <h2>Search</h2>
-        <p>{routeParams.id}</p>
-        {identity && <p>Ethereum Address: {identity.owner}</p>}
+      <View>
+        {identity && (
+          <Card backgroundColor="primary">
+            <Image
+              backgroundSize="cover"
+              display="inline-block"
+              margin={[0, '16px', 0, 0]}
+              src="/img/icon-person.svg"
+              size={['32px']}
+              verticalAlign="middle"
+            />
+
+            <Text
+              color="white"
+              display="inline-block"
+              fontSize="18px"
+              fontWeight={700}
+            >
+              {routeParams.id} ({metaId.getTruncatedMetaIdOwner(identity.owner)})
+            </Text>
+          </Card>
+        )}
+
         {claims &&
           claims.map((claim, key) => (
-            <div key={key}>
-              <h4>Claim: {claim.claim}</h4>
+            <Card key={key}>
+              <Box align="middle">
+                <Image
+                  backgroundSize="cover"
+                  margin={[0, '16px', 0, 0]}
+                  src="/img/icon-verified-claim.svg"
+                  size={['32px']}
+                />
 
-              <p>
-                Claim verified by:&nbsp;
-                <Link to={`${routes.search.path}/${claim.issuer}`}>
-                  {claim.issuer}
-                </Link>
-              </p>
-            </div>
+                <Box flexDirection="column">
+                  <Text fontWeight={700}>Claim: {claim.claim}</Text>
+
+                  <Text>
+                    Verified by:&nbsp;
+                    <Link to={`${routes.search.path}/${claim.issuer}`}>
+                      {metaId.getTruncatedMetaIdOwner(claim.issuer)}
+                    </Link>
+                  </Text>
+                </Box>
+              </Box>
+            </Card>
           ))}
+
         {isSessionAccount && (
           <Link to={routes.claim.path}>
-            <button>New Claim</button>
+            <Button
+              backgroundColor="primary"
+              borderRadius="40px"
+              borderWidth="0"
+              margin={['32px', 'auto']}
+              padding={['16px', '24px']}
+            >
+              <Text color="white" fontWeight={700}>
+                Add a new claim
+              </Text>
+            </Button>
           </Link>
         )}
-      </div>
+      </View>
     )
   }
 }
