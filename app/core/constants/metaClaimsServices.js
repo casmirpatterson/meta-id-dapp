5225a02a570e363d9969b756541a79a189307618
lukehedger <luke.hedger@gmail.com>
2017-12-08 16:26:04 +0000

    Update MCS config

--- app/core/constants/metaClaimsServices.js
+++ app/core/constants/metaClaimsServices.js
@@ -1,11 +1,11 @@
 export default {
   jaak: {
-    address: '0xF540Ecfd7516c73596F27309bC7A28a56f2ee040',
+    address: '0xE4258268bf30F9540EeBfF7150148E387bcE0a2f',
     displayName: 'JAAK',
     url: 'http://localhost:7000',
   },
   spotify: {
-    address: '0xF540Ecfd7516c73596F27309bC7A28a56f2ee040',
+    address: '0xE579C07F0b34ec373C79C8072FC0B0Bc7d75EB7D',
     displayName: 'Spotify',
     url: 'http://localhost:8000',
   },
