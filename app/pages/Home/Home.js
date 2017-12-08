4226ff038af1375fab527230ec5cba73bd262144
lukehedger <luke.hedger@gmail.com>
2017-12-08 13:07:59 +0000

    Update size of new meta-id button

--- app/pages/Home/Home.js
+++ app/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Box } from 'jaak-primitives'
 
 import { Link } from 'core/components'
 import { Button, Image, Text, View } from 'core/primitives'
@@ -18,32 +19,34 @@ class Home extends Component {
       <View>
         <Components.Search submitSearch={this.onSubmitSearch} />
 
-        <Link to={routes.register.path}>
-          <Button
-            backgroundColor="primary"
-            borderRadius="50%"
-            borderWidth="0"
-            boxShadow="0px 4px 12px rgba(0, 0, 0, 0.4)"
-            display="flex"
-            margin={[0, 'auto']}
-            size={['48px']}
-          >
-            <Image
-              backgroundSize="cover"
-              size={['24px']}
-              src="/img/icon-add.svg"
-            />
-          </Button>
+        <Box margin={[0, 'auto']} size={['auto', '148px']}>
+          <Link to={routes.register.path}>
+            <Button
+              backgroundColor="primary"
+              borderRadius="50%"
+              borderWidth="0"
+              boxShadow="0px 4px 12px rgba(0, 0, 0, 0.4)"
+              display="flex"
+              margin={[0, 'auto']}
+              size={['48px']}
+            >
+              <Image
+                backgroundSize="cover"
+                size={['24px']}
+                src="/img/icon-add.svg"
+              />
+            </Button>
 
-          <Text
-            fontSize="18px"
-            fontWeight={700}
-            margin={['16px', 0, 0]}
-            textAlign="center"
-          >
-            Create META-ID
-          </Text>
-        </Link>
+            <Text
+              fontSize="18px"
+              fontWeight={700}
+              margin={['16px', 0, 0]}
+              textAlign="center"
+            >
+              Create META-ID
+            </Text>
+          </Link>
+        </Box>
       </View>
     )
   }
