8330350f6074c61797984cefe98f537c59ad8fe9
lukehedger <luke.hedger@gmail.com>
2017-12-08 12:28:35 +0000

    Add create meta-id button to Home page

--- app/pages/Home/Home.js
+++ app/pages/Home/Home.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react'
 
 import { Link } from 'core/components'
+import { Button, Image, Text, View } from 'core/primitives'
 import { routes } from 'core/routes'
 import * as Components from './components'
 
 class Home extends Component {
   render() {
-    return (
-      <div>
-        <h1>META-ID ÐApp</h1>
+    console.log(this.props)
 
+    return (
+      <View>
         <Components.Search />
 
         <Link to={routes.register.path}>
-          <button>Register</button>
-        </Link>
+          <Button
+            backgroundColor="primary"
+            borderRadius="50%"
+            borderWidth="0"
+            boxShadow="0px 4px 12px rgba(0, 0, 0, 0.4)"
+            display="flex"
+            margin={[0, 'auto']}
+            size={['48px']}
+          >
+            <Image
+              backgroundSize="cover"
+              size={['24px']}
+              src="/img/icon-add.svg"
+            />
+          </Button>
 
-        <Link to={routes.login.path}>
-          <button>Login</button>
+          <Text
+            fontSize="18px"
+            fontWeight={700}
+            margin={['16px', 0, 0]}
+            textAlign="center"
+          >
+            New META-ID
+          </Text>
         </Link>
-      </div>
+      </View>
     )
   }
 }
