2335150772f4577fb22398f9aed951fb668e5ca9
lukehedger <luke.hedger@gmail.com>
2017-12-20 16:52:29 +0000

    Update services exports with Swarm service

--- app/core/services/index.js
+++ app/core/services/index.js
@@ -2,6 +2,7 @@ import * as Fetch from './fetch'
 import * as MetaClaims from './meta-claims'
 import * as MetaId from './meta-id'
 import * as MetaNetwork from './meta-network'
+import * as Swarm from './swarm'
 import * as WebStorage from './web-storage'
 
-export { Fetch, MetaClaims, MetaId, MetaNetwork, WebStorage }
+export { Fetch, MetaClaims, MetaId, MetaNetwork, Swarm, WebStorage }
