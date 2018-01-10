95e43809f4429bd15fba14983e80ed0aeda367c9
lukehedger <luke.hedger@gmail.com>
2018-01-10 16:22:57 +0000

    Add MetaClaimsService component

--- app/core/components/index.js
+++ app/core/components/index.js
@@ -1,8 +1,17 @@
 import Link from './Link'
 import Logo from './Logo'
+import MetaClaimsService from './MetaClaimsService'
 import MetaIdDisplay from './MetaIdDisplay'
 import MetaIdentity from './MetaIdentity'
 import MetaIdInput from './MetaIdInput'
 import Search from './Search'
 
-export { Link, Logo, MetaIdDisplay, MetaIdentity, MetaIdInput, Search }
+export {
+  Link,
+  Logo,
+  MetaClaimsService,
+  MetaIdDisplay,
+  MetaIdentity,
+  MetaIdInput,
+  Search,
+}
