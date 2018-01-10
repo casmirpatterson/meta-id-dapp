8314c45f1bae65ecd2ea81e736b86f2e542fab7d
lukehedger <luke.hedger@gmail.com>
2018-01-10 14:39:53 +0000

    Update MetaIdDisplay component styles

--- app/core/components/MetaIdDisplay/MetaIdDisplay.js
+++ app/core/components/MetaIdDisplay/MetaIdDisplay.js
@@ -6,8 +6,8 @@ import { metaId } from 'core/util'
 
 const MetaIdDisplay = ({ username }) => (
   <Fragment>
-    <Span>{metaId.getNameFromUsername(username)}</Span>
-    <Span color="accent">{META_ID_USERNAME_SUFFIX}</Span>
+    <Span color="accent">{metaId.getNameFromUsername(username)}</Span>
+    <Span color="grey">{META_ID_USERNAME_SUFFIX}</Span>
   </Fragment>
 )
 
