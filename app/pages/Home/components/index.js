94db7faa285deb05c4ae58452e701b674e7ebf4d
lukehedger <luke.hedger@gmail.com>
2018-01-10 16:23:24 +0000

    Add Home page Claims component

--- app/pages/Home/components/index.js
+++ app/pages/Home/components/index.js
@@ -1,5 +1,6 @@
 import Anonymous from './Anonymous.js'
+import Claims from './Claims.js'
 import Onymous from './Onymous.js'
 import SetupMetaId from './SetupMetaId.js'
 
-export { Anonymous, Onymous, SetupMetaId }
+export { Anonymous, Claims, Onymous, SetupMetaId }
