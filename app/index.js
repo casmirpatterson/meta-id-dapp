8ea11c743ea9d0f3e9aead985ae771000db13b26
lukehedger <luke.hedger@gmail.com>
2018-01-29 18:28:44 +0000

    Disable Sentry logging in development [Fixes #154735037]

--- app/index.js
+++ app/index.js
@@ -3,12 +3,13 @@ import { render } from 'react-dom'
 
 import Root from 'core/containers/Root'
 import { Sentry } from 'core/services'
+import { isDevelopment } from 'core/util'
 
 import 'core/style/global'
 
 const rootEl = document.getElementById('Root')
 
 // init Sentry
-Sentry.install()
+if (!isDevelopment) Sentry.install()
 
 render(<Root />, rootEl)
