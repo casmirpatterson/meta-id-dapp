10a721098a775f5fc19d210c580f082e575a30ff
lukehedger <luke.hedger@gmail.com>
2018-02-05 14:56:57 +0000

    Add session state storage logic to middleware

--- app/core/middleware/session.js
+++ app/core/middleware/session.js
@@ -1,9 +1,14 @@
 import { Actions as farce } from 'farce'
 
+import { STATE_KEY } from 'core/constants'
 import { routes } from 'core/routes'
 import { isDomainAction } from 'core/util'
-import { actions as identity } from 'domains/identity'
-import { actionTypes as session, name } from 'domains/session'
+import { actions as IdentityActions } from 'domains/identity'
+import {
+  actions as SessionActions,
+  actionTypes as session,
+  name,
+} from 'domains/session'
 
 const SessionMiddleware = ({ dispatch }) => next => action => {
   // action not in namespace? abort!
@@ -11,10 +16,24 @@ const SessionMiddleware = ({ dispatch }) => next => action => {
 
   if (session.LOGIN === action.type) {
     // fetch user's META Identity
-    dispatch(identity.readIdentityByOwner(action.payload.account.address))
+    dispatch(
+      IdentityActions.readIdentityByOwner(action.payload.account.address)
+    )
 
     // redirect to Home page
     dispatch(farce.push(routes.home.path))
+
+    // store `session.account` state
+    dispatch(
+      SessionActions.setStoredSession(STATE_KEY, {
+        session: { account: action.payload.account },
+      })
+    )
+  }
+
+  if (session.LOGOUT === action.type) {
+    // delete stored `session.account` state
+    dispatch(SessionActions.removeStoredSession(STATE_KEY))
   }
 
   return next(action)
