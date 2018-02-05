3fc91ccc94906a05fb3f19c8f101e5d2c3bcb025
lukehedger <luke.hedger@gmail.com>
2018-02-05 14:55:22 +0000

    Add session domain actions for IndexedDB storage

--- app/domains/session/actionTypes.js
+++ app/domains/session/actionTypes.js
@@ -1,6 +1,9 @@
 import { name } from './constants'
 
+export const GET_STORED_SESSION = `${name}/GET_STORED_SESSION`
 export const LOGIN = `${name}/LOGIN`
 export const LOGOUT = `${name}/LOGOUT`
+export const REMOVE_STORED_SESSION = `${name}/REMOVE_STORED_SESSION`
 export const SET_IS_NEW_USER = `${name}/SET_IS_NEW_USER`
 export const SET_OAUTH_CLAIM_MESSAGE = `${name}/SET_OAUTH_CLAIM_MESSAGE`
+export const SET_STORED_SESSION = `${name}/SET_STORED_SESSION`
